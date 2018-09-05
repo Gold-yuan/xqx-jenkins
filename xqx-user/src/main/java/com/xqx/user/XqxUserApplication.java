3e5545fd8c9e4ae7e1cd464a059003f8b872f9f2
yuan <bjyuantf@cn.ibm.com>
2018-09-05 14:56:33 +0800

    tomcat 启动spring boot

--- xqx-user/src/main/java/com/xqx/user/XqxUserApplication.java
+++ xqx-user/src/main/java/com/xqx/user/XqxUserApplication.java
@@ -2,9 +2,17 @@ package com.xqx.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class XqxUserApplication {
+public class XqxUserApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// tomcat启动时 Class为标注有@SpringBootApplication的主启动类
+		return application.sources(XqxUserApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(XqxUserApplication.class, args);
