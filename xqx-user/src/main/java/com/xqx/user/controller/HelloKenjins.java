545b1dc048e8c0f0013c76221b43a68ceeccbff6
yuan <bjyuantf@cn.ibm.com>
2018-09-12 15:41:14 +0800

    test

--- xqx-user/src/main/java/com/xqx/user/controller/HelloKenjins.java
+++ xqx-user/src/main/java/com/xqx/user/controller/HelloKenjins.java
@@ -10,7 +10,7 @@ public class HelloKenjins {
 	@GetMapping(value = "/hello")
 	public String hello() {
 		
-		return "hello luck boy! This is next version!2018-09-11测试 233";
+		return "hello luck boy! This is next version!2018-09-11测试 233!2018-09-12";
 	}
 	@RequestMapping(value = "/hello2")
 	public String hello2() {
